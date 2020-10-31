0016d2a4976fbbe25a6720ad6feb707ba2d2914a
贾培桐 <jiapeitong@jiapeitongdeMacBook-Pro.local>
2020-10-31 10:43:43 +0800

    homework1031

---
+++ test93.java
@@ -0,0 +1,17 @@
+package chapter9;
+
+public class test93 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long i = 10000;
+		for(int j = 0; j<8;j++) {
+			System.out.println(new java.util.Date(i).toString());
+			i=i*10;
+		}
+			
+			
+		
+
+	}
+
+}
